c991c72556662d1a291c29719ad86d7a8e43c025
Torres62 <soares.rtorres@gmail.com>
2020-02-20 17:06:37 -0300

    a

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -17,26 +17,26 @@ class AccountListState extends State<AccountList>{
     return Center(
       child: ListWheelScrollView(
         itemExtent: 80, 
-        diameterRatio: 1.5,
+        diameterRatio: 1.6,
         children: <Widget>[
           Container(
-            width: 300,
+            width: 330,
             color: Colors.black,
           ),
           Container(
-            width: 300,
+            width: 330,
             color: Colors.green,
           ),
           Container(
-            width: 300,
+            width: 330,
             color: Colors.purple,
           ),
           Container(
-            width: 300,
+            width: 330,
             color: Colors.red,
           ),
           Container(
-            width: 300,
+            width: 330,
             color: Colors.blue,
           ),
         ]
