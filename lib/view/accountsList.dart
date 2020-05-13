fb5cc14a45e4b12a72209f09f24ac32bdb2e9469
Torres62 <soares.rtorres@gmail.com>
2020-05-12 22:27:50 -0300

    Updating profiles list

--- lib/view/accountsList.dart
+++ lib/view/accountsList.dart
@@ -10,7 +10,9 @@ class AccountsListState extends State<AccountsList>{
   @override
   Widget build(BuildContext context) {    
     return Scaffold(
-      body: _buildListBody()
+      body: _buildListBody(),
+      drawer: _buildDrawer(),
+      appBar: AppBar(),
     );
   }
 
@@ -39,6 +41,68 @@ class AccountsListState extends State<AccountsList>{
     );
   }
 
+  Widget _buildDrawer(){
+    return Drawer(      
+      child: ListView(
+        children: <Widget>[
+          DrawerHeader(            
+            child: Text(
+              'CRYPTO MANAGER',
+              style: TextStyle(
+                color: ColorConverter().firstButtonGradient().withOpacity(0.8),
+                fontSize: 24
+              ),
+            )
+          ),          
+          _buildDrawerProfile(),
+          _buildDrawerLogout()
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDrawerProfile(){
+    return GestureDetector(
+      onTap: (){},
+      child: Container(
+        child: ListTile(
+          leading: Icon(
+            Icons.person,
+            color: ColorConverter().firstButtonGradient().withOpacity(0.8),
+          ),
+          title: Text(
+            'Profile',
+            style: TextStyle(
+              color: ColorConverter().firstButtonGradient().withOpacity(0.8),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDrawerLogout(){
+    return GestureDetector(
+      onTap: (){
+        Navigator.of(context).pushNamed('/signHome');
+      },
+      child: Container(
+        child: ListTile(
+          leading: Icon(
+            Icons.exit_to_app, 
+            color: Colors.red,
+          ),
+          title: Text(
+            'Logout', 
+            style: TextStyle(
+              color: Colors.red
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget _buildProfilesList(){
     return Flexible(
       child: Padding(
@@ -49,6 +113,7 @@ class AccountsListState extends State<AccountsList>{
             return Padding(
               padding: const EdgeInsets.all(8.0),
               child: Container(                
+                height: 80,
                 decoration: BoxDecoration(
                   color: Colors.transparent,
                   borderRadius: BorderRadius.circular(16),
